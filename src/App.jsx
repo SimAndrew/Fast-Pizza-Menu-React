63c80d269240f320a268732b52c196387e29a53e
AndSim <siminiucandrew@gmail.com>
2023-12-13 14:03:17 +0200

    feat: render pizzaData (menu) in ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import './App.css';
 
+const pizzaData = [
+	{
+		name: 'Focaccia',
+		ingredients: 'Bread with italian olive oil and rosemary',
+		price: 6,
+		photoName: 'pizzas/focaccia.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Margherita',
+		ingredients: 'Tomato and mozarella',
+		price: 10,
+		photoName: 'pizzas/margherita.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Spinaci',
+		ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
+		price: 12,
+		photoName: 'pizzas/spinaci.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Funghi',
+		ingredients: 'Tomato, mozarella, mushrooms, and onion',
+		price: 12,
+		photoName: 'pizzas/funghi.jpg',
+		soldOut: false,
+	},
+	{
+		name: 'Pizza Salamino',
+		ingredients: 'Tomato, mozarella, and pepperoni',
+		price: 15,
+		photoName: 'pizzas/salamino.jpg',
+		soldOut: true,
+	},
+	{
+		name: 'Pizza Prosciutto',
+		ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
+		price: 18,
+		photoName: 'pizzas/prosciutto.jpg',
+		soldOut: false,
+	},
+];
+
 export function App() {
 	return (
 		<div className={'container'}>
@@ -23,32 +68,26 @@ function Menu() {
 	return (
 		<main className={'menu'}>
 			<h2>Our menu</h2>
-			<Pizza
-				name="Pizza Spinaci"
-				ingredients="Tomato, mozarella, spinach, and ricotta cheese"
-				photoName="pizzas/spinaci.jpg"
-				price={10}
-			/>
-			<Pizza
-				name="Pizza Funghi"
-				ingredients="Tomato, mozarella, mushrooms, and onion"
-				photoName="pizzas/funghi.jpg"
-				price={12}
-			/>
+
+			<ul className="pizzas">
+				{pizzaData.map((pizza) => (
+					<Pizza pizzaObj={pizza} key={pizza.name} />
+				))}
+			</ul>
 		</main>
 	);
 }
 
 function Pizza(props) {
 	return (
-		<div className="pizza">
-			<img src={props.photoName} alt={props.name} />
+		<li className="pizza">
+			<img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
 			<div>
-				<h3>{props.name}</h3>
-				<p>{props.ingredients}</p>
-				<span>{props.price}</span>
+				<h3>{props.pizzaObj.name}</h3>
+				<p>{props.pizzaObj.ingredients}</p>
+				<span>{props.pizzaObj.price}</span>
 			</div>
-		</div>
+		</li>
 	);
 }
 
