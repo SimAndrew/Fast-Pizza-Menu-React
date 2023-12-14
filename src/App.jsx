36e6afbdfe3044ede146f3ccba5a4955a2c9dae6
AndSim <siminiucandrew@gmail.com>
2023-12-14 16:08:02 +0200

    refactor: destructuring props (Pizza, Order)

--- src/App.jsx
+++ src/App.jsx
@@ -85,15 +85,15 @@ function Menu() {
 	);
 }
 
-function Pizza(props) {
-	if (props.pizzaObj.soldOut) return null;
+function Pizza({ pizzaObj }) {
+	if (pizzaObj.soldOut) return null;
 	return (
 		<li className="pizza">
-			<img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
 			<div>
-				<h3>{props.pizzaObj.name}</h3>
-				<p>{props.pizzaObj.ingredients}</p>
-				<span>{props.pizzaObj.price}</span>
+				<h3>{pizzaObj.name}</h3>
+				<p>{pizzaObj.ingredients}</p>
+				<span>{pizzaObj.price}</span>
 			</div>
 		</li>
 	);
@@ -119,12 +119,12 @@ function Footer() {
 	);
 }
 
-function Order(props) {
+function Order({ openHour, closeHour }) {
 	return (
 		<div className="order">
 			<p>
-				We are open until {props.openHour}:00 until {props.closeHour}:00. Come
-				visit us or order online.
+				We are open from {openHour}:00 to {closeHour}:00. Come visit us or order
+				online.
 			</p>
 			<button className="btn">Order</button>
 		</div>
